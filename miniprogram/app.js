2fecd23e4b0daf905cc1cabf0194eb72bf87720c
qin-ziqi <qinziqi@lechebang.com>
2019-01-15 18:34:38 +0800

    初版提交

---
+++ miniprogram/app.js
@@ -0,0 +1,52 @@
+//app.js
+App({
+    onLaunch: function() {
+        wx.cloud.init({
+            env: 'dev-38f534',
+            traceUser: true
+        })
+
+        this.checkUpdates()
+        this.getUserInfo()
+    },
+    /**
+     * 检测版本更新
+     */
+    checkUpdates() {
+        const updateManager = wx.getUpdateManager()
+        updateManager.onUpdateReady(function() {
+            wx.showModal({
+                title: '更新提示',
+                content: '新版本已经准备好，是否重启应用？',
+                success(res) {
+                    if (res.confirm) {
+                        updateManager.applyUpdate()
+                    }
+                }
+            })
+        })
+    },
+    /**
+     * 获取用户登录信息
+     */
+    getUserInfo() {
+        // 调用云函数
+        wx.cloud.callFunction({
+            name: 'login',
+            data: {},
+            success: res => {
+                console.log('[云函数] [login] user openid: ', res.result.openid)
+				this.globalData.openid = res.result.openid
+            },
+            fail: err => {
+                console.error('[云函数] [login] 调用失败', err)
+            }
+        })
+    },
+	/**
+	 * 全局信息
+	 */
+    globalData: {
+        openid: null
+    }
+})
---
+++ miniprogram/utils/util.js
@@ -0,0 +1,63 @@
+/*
+ * 工具类方法
+ * Written by Qinziqi
+ */
+
+module.exports = {
+    /**
+     * 错误提醒
+     * @param {string} msg 错误提示信息
+     * @return {void}
+     */
+    errorHandler: msg => {
+        wx.showToast({
+            title: msg,
+            icon: 'none',
+            duration: 1000
+        })
+    },
+
+    /**
+     * 判断对象是否为空
+     * @param {Object} obj 需要检测的对象
+     * @return {boolean} 检测的结果
+     */
+    isEmptyObject: obj => {
+        for (let i in obj) {
+            return false
+        }
+        return true
+    },
+
+    /**
+     * 格式化日期
+     * @param {Date} date 传入的日期对象
+     * @param {string} type 所需要的日期类型 例：'yyyy-MM-dd'
+     * @return {string} 格式化后的日期 例：'2019-01-15'
+     */
+    formatDate(date, type) {
+        if (isNaN(date.getTime())) {
+            return '--';
+        }
+
+        const o = {
+            'y+': date.getFullYear(),
+            'M+': date.getMonth() + 1,
+            'd+': date.getDate(),
+            'h+': date.getHours(),
+            'm+': date.getMinutes(),
+            's+': date.getSeconds(),
+            // 季度
+            'q+': Math.floor((date.getMonth() + 3) / 3),
+            // 毫秒
+            'S+': date.getMilliseconds()
+        }
+
+        for (let k in o) {
+            if (new RegExp(`(${k})`).test(type)) {
+                type = type.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+            }
+        }
+        return type;
+    }
+}
